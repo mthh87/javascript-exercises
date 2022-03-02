4e42ef16e646a4361ce6f93fac69d7c8b4bd0825
mthh87 <matt@harper-house.net>
2022-03-01 19:39:48 -0800

    finished 01-04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, cnt) {
+    
+    if (cnt < 0) return 'ERROR';
+    let tmpStr = '';
+    for (let i = 1; i <= cnt; i++) {
+        tmpStr += str;
+    }
+    return tmpStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(orgArray,...args) {
+//    console.log(orgArray);
+//    console.log(args);
+    for (let i = 0; i < args.length; i++) {
+        let newArray = orgArray.filter((aryElem) => (aryElem !== args[i]));
+//        console.log(newArray);
+        orgArray = [...newArray];
+    }
+//    console.log('all done...returning');
+    return orgArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+//console.log('be carefull down there');
+//console.log(removeFromArray([1,2,3,4],3));
