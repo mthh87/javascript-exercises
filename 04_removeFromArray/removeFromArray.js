0cf5c4b6024425759ba30a349a4ed7162cf3a997
mthh87 <matt@harper-house.net>
2022-03-04 14:00:14 -0800

    updated 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,10 @@
 const removeFromArray = function(orgArray,...args) {
-//    console.log(orgArray);
-//    console.log(args);
     for (let i = 0; i < args.length; i++) {
         let newArray = orgArray.filter((aryElem) => (aryElem !== args[i]));
-//        console.log(newArray);
         orgArray = [...newArray];
     }
-//    console.log('all done...returning');
     return orgArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
-//console.log('be carefull down there');
-//console.log(removeFromArray([1,2,3,4],3));
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if ((num1 < 0) | (num2 < 0)) {return 'ERROR'}
+    if ((typeof(num1) !== 'number') | (typeof(num2) !== 'number')) {return 'ERROR'}
+    let var1 = 0;
+    let var2 = 0;
+
+    if (num1 > num2) {
+        var1 = num2;
+        var2 = num1;
+    } else {
+        var1 = num1;
+        var2 = num2;
+    }
+
+    let sum = 0;
+
+    for (let i = var1; i <= var2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
